35c65b0dcc4f987bbc0e587e1bc10be092e43e68
Ryan Resella <ryan@codeforamerica.org>
2011-09-05 23:27:44 -0700

    Revert "Added connexionz client in app controller"
    This reverts commit 35f3c7368baf6512a31fb9ca069d48df8bd16f0c.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  private
-
-  def connexionz
-    @client = Connexionz::Client.new({:endpoint => "http://businfo.santa-clarita.com/"})
-  end
-
 end
