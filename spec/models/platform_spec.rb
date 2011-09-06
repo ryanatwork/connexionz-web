526202407a6982a4d1efb42f2f0a6f656e701b66
Ryan Resella <ryan@codeforamerica.org>
2011-09-05 21:27:58 -0700

    Added spec for add_platforms

--- spec/models/platform_spec.rb
+++ spec/models/platform_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Platform do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "add_platforms" do
+    before do
+      @test = Platform.new.add_platforms
+    end
+
+    it "should add platforms to the database" do
+      @test.should_not be_nil
+    end
+  end
+
 end
