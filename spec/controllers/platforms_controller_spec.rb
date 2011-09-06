d04bccbeefda82f811e27d53d07688c2adbb6b5c
Ryan Resella <ryan@codeforamerica.org>
2011-09-05 21:21:12 -0700

    Wrote specs for show

--- spec/controllers/platforms_controller_spec.rb
+++ spec/controllers/platforms_controller_spec.rb
@@ -7,6 +7,18 @@ describe PlatformsController do
       get 'index'
       response.should be_success
     end
+
+    it "should redirect to show path" do
+      get 'index', :id => '10279'
+      response.should redirect_to platform_path(10279)
+    end
+  end
+
+  describe "GET 'show'" do
+    it "should be successful" do
+      get :show, :id => '10279'
+      response.should render_template("platforms/show")
+    end
   end
 
 end
