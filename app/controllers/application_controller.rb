35f3c7368baf6512a31fb9ca069d48df8bd16f0c
Ryan Resella <ryan@codeforamerica.org>
2011-09-05 23:27:13 -0700

    Added connexionz client in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def connexionz
+    @client = Connexionz::Client.new({:endpoint => "http://businfo.santa-clarita.com/"})
+  end
+
 end
